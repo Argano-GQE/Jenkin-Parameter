b1fcd068a665477fb0fa1ddf33f566a4f8a3a8db
Argano-GQE <ravi.nelluri@argano.com>
2023-07-14 18:34:08 +0530

    Pom.xml with Parameter

---
+++ src/main/java/com/qa/webapp/utils/FrameworkExcepetion.java
@@ -0,0 +1,11 @@
+package com.qa.webapp.utils;
+
+public class FrameworkExcepetion extends RuntimeException{
+
+	public FrameworkExcepetion(String mesg) {
+		
+		super(mesg);
+		
+	}
+	
+}
---
+++ src/test/java/com/qa/webapp/testcases/LoginPage.java
@@ -0,0 +1,44 @@
+package com.qa.webapp.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
+import com.qa.webapp.utils.*;
+
+
+public class LoginPage {
+
+	// static WebDriver driver;
+	@Parameters("Browser")
+	@Test
+	public static void application() {
+
+		By username = By.name("user-name");
+		By password = By.name("password");
+		By Login = By.xpath("//input[@id='login-button']");
+		By menu = By.xpath("//button[@id='react-burger-menu-btn']");
+		By logout = By.xpath("//a[@id='logout_sidebar_link']");
+
+		BrowserUtil brUtl = new BrowserUtil();
+
+		WebDriver driver = brUtl.launchBrowser("Browser");
+		brUtl.enterURL("https://www.saucedemo.com/");
+		brUtl.getPageTitle();
+		brUtl.getCurrentURL();
+
+		ElementUtl eleUtil = new ElementUtl(driver);
+
+		eleUtil.sendKeys(username, "standard_user");
+		eleUtil.sendKeys(password, "secret_sauce");
+		eleUtil.doClick(Login);
+		eleUtil.doClick(menu);
+		eleUtil.doClick(logout);
+		
+		System.out.println("**********----- Login and Logout Successfully-------********");
+
+	}
+
+}
